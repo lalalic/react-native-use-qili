cf416908c32c39f80540dbca303dc02600682ccb
lalalic <lalalic@139.com>
2023-07-06 10:44:15 -0400

    fix

--- cloud/expo-updates.js
+++ cloud/expo-updates.js
@@ -2,8 +2,8 @@ module.exports=(updates="updates")=>({
     name:"qili-expo-updates",
     appUpdates:{
         context:updates,
-        fromManifestURI({runtimeVersion, platform, app}){
-           return `https://cdn.qili2.com/${app.apiKey}/${updates}/${runtimeVersion}/${platform}-manifest.json`
+        fromManifestURI({runtimeVersion, platform}, app){
+           return `https://cdn.qili2.com/${app.app.apiKey}/${updates}/${runtimeVersion}/${platform}-manifest.json`
         }
     }
 })

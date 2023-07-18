1915b651d1c00fee70303bed5c883b176e03fd83
lalalic <lalalic@139.com>
2023-07-18 19:26:06 -0400

    singleton pubsub

--- cloud/web-proxy.js
+++ cloud/web-proxy.js
@@ -1,91 +1,105 @@
 const { withFilter }=require("graphql-subscriptions")
 let uuid=Date.now()
+let pubsub
+/**
+ * pubsub engine doesn't support changing on-fly since it's unsafe to close
+ * each app should only have one qili-web-proxy module, so module pubsub is safe enough in a context vm
+ */
 
-module.exports=(pubsub,topic="default")=>({
-    name:"qili-web-proxy",
-    typeDefs:`
-        extend type Query{
-            answerHelp(session:String!, response:JSON!):JSON
-        }
+module.exports=(singlePubsub,topic="default")=>{
+    if(pubsub){
+        singlePubsub?.close?.()
+    }else{
+        pubsub=singlePubsub
+    }
+    return {
+        name:"qili-web-proxy",
+        typeDefs:`
+            extend type Query{
+                answerHelp(session:String!, response:JSON!):JSON
+            }
 
-        extend type Subscription{
-            askThenWaitAnswer(message:JSON!):JSON
-            helpQueue(helper:String):JSON
-        }
-    `,
-    resolver:{
-        Query:{
-            answerHelp(_,{session, response}, {app}){
-                app.pubsub.publish(`${app.app.apiKey}.${topic}.answer`, {session, response})
-                return true
-            },
-        },
-        Subscription:{
-            askThenWaitAnswer:{
-                subscribe(_,{message}, {app, user}){
-                    if(Helpers.no){
-                        app.logger.info('No helper, discard an ask')
-                        throw new Error("Your request can't be processed now!")
-                    }
-                    const ask={session:`${++uuid}`,message}
-                    app.pubsub.publish(`${app.app.apiKey}.${topic}.ask`, ask)
-                    return withFilter(
-                        ()=>app.pubsub.asyncIterator([`${app.app.apiKey}.${topic}.answer`]),
-                        answer=>{
-                            const answered=answer.session==ask.session
-                            if(answered){
-                                app.logger.info(`ask[${answer.session}] is answered and returned to asker`)
-                            }
-                            return answered
-                        },
-                    )(...arguments)
+            extend type Subscription{
+                askThenWaitAnswer(message:JSON!):JSON
+                helpQueue(helper:String):JSON
+            }
+        `,
+        resolver:{
+            Query:{
+                answerHelp(_,{session, response}, {app}){
+                    app.pubsub.publish(`${app.app.apiKey}.${topic}.answer`, {session, response})
+                    return true
                 },
-                resolve(answer){
-                    return answer.response
-                }
             },
-
-            helpQueue:{
-                subscribe(_,{helper}, {app,user}){
-                    Helpers.add(helper=user._id||helper)
-                    return withFilter(
-                        ()=>app.pubsub.asyncIterator([`${app.app.apiKey}.${topic}.ask`]),
-                        ask=>{
-                            const picked=Helpers.pick1(ask)===helper
-                            if(picked){
-                                app.logger.info(`ask[${ask.session}] is send to helper[${helper}]`)
-                            }
-                            return picked
+            Subscription:{
+                askThenWaitAnswer:{
+                    subscribe(_,{message}, {app, user}){
+                        if(Helpers.no){
+                            app.logger.info('No helper, discard an ask')
+                            throw new Error("Your request can't be processed now!")
                         }
-                    )(...arguments)
+                        const ask={session:`${++uuid}`,message}
+                        app.pubsub.publish(`${app.app.apiKey}.${topic}.ask`, ask)
+                        return withFilter(
+                            ()=>app.pubsub.asyncIterator([`${app.app.apiKey}.${topic}.answer`]),
+                            answer=>{
+                                const answered=answer.session==ask.session
+                                if(answered){
+                                    app.logger.info(`ask[${answer.session}] is answered and returned to asker`)
+                                }
+                                return answered
+                            },
+                        )(...arguments)
+                    },
+                    resolve(answer){
+                        return answer.response
+                    }
                 },
-                resolve(ask,{},{app,user}){
-                    Helpers.done1(ask)
-                    return ask
+
+                helpQueue:{
+                    subscribe(_,{helper}, {app,user}){
+                        Helpers.add(helper=user._id||helper)
+                        return withFilter(
+                            ()=>app.pubsub.asyncIterator([`${app.app.apiKey}.${topic}.ask`]),
+                            ask=>{
+                                const picked=Helpers.pick1(ask)===helper
+                                if(picked){
+                                    app.logger.info(`ask[${ask.session}] is send to helper[${helper}]`)
+                                }
+                                return picked
+                            }
+                        )(...arguments)
+                    },
+                    resolve(ask,{},{app,user}){
+                        Helpers.done1(ask)
+                        return ask
+                    }
                 }
             }
-        }
-    },
-    pubsub:{
-        init(){
-            return pubsub
         },
-        onDisconnect({app,user, request}){
-            switch(request?.id){
-                case "helpQueue":
-                    Helpers.remove(user._id)
-                    Helpers.remove(request.variables.helper)
-                break
+        pubsub:{
+            init(){
+                return pubsub
+            },
+            onDisconnect({app,user, request}){
+                switch(request?.id){
+                    case "helpQueue":
+                        Helpers.remove(user._id)
+                        Helpers.remove(request.variables.helper)
+                    break
+                }
             }
-        }
-    },
-        
-    finalize(app){
-        if(pubsub && !pubsub.closed){
-            pubsub.close?.()
+        },
+            
+        finalize(app){
+            // if(pubsub && !pubsub.closed){
+            //     pubsub.publish(`${app.app.apiKey}.${topic}.answer`,{closed:true})
+            //     pubsub.publish(`${app.app.apiKey}.${topic}.ask`,{closed:true})
+            //     pubsub.close?.()
+            // }
         }
     }
-})
+}
 
 class Helpers{
     constructor(){

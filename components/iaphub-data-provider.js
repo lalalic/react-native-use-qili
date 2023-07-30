ae7aed642d7ac9c54e19a89c7a3a0ca050d505e6
lalalic <lalalic@139.com>
2023-07-30 13:31:13 -0400

    extended

--- components/iaphub-data-provider.js
+++ components/iaphub-data-provider.js
@@ -1,12 +1,58 @@
 import React from "react";
 import Iaphub from "react-native-iaphub"
 import { IaphubDataProvider } from 'react-native-iaphub-ui';
+import iap from "react-native-iaphub-ui/src/i18n/en"
 
-export default function MyIaphubDataProvider({products,onPurchase, ...props}) {
+export default function MyIaphubDataProvider({products,onPurchase, l10n, ...props}) {
     Iaphub.products=products
+
+    React.useMemo(()=>{
+        Object.keys(iap).forEach(key=>{
+            Object.keys(iap[key]).forEach(k=>{
+                iap[key][k]=(fx=>(...args)=>l10n[fx(...args)])(iap[key][k])
+            })
+        })
+        return true
+    },[])
+
     React.useEffect(() =>{
-        onPurchase && Iaphub.addEventListener('onPurchase', onPurchase)
+        onPurchase && Iaphub.addEventListener('onPurchase', onPurchase);
         return () => Iaphub.end?.()
     },[]);
-    return <IaphubDataProvider {...{ appId: "A", apiKey: "A", userId: "A",}} {...props} />;
+    return <IaphubDataProvider {...{ appId: "A", apiKey: "A", userId: "A", lang:l10n.getLanguage()}} {...props} />;
 }
+
+
+import React from "react";
+import Iaphub from "react-native-iaphub"
+import { IaphubDataProvider } from 'react-native-iaphub-ui';
+import iap from "react-native-iaphub-ui/src/i18n/en"
+
+export default function MyIaphubDataProvider({products,onPurchase, l10n, ...props}) {
+    Iaphub.products=products
+
+    React.useMemo(()=>{
+        Object.keys(iap).forEach(key=>{
+            Object.keys(iap[key]).forEach(k=>{
+                iap[key][k]=(fx=>(...args)=>l10n[fx(...args)])(iap[key][k])
+            })
+        })
+        return true
+    },[])
+
+    React.useEffect(() =>{
+        onPurchase && Iaphub.addEventListener('onPurchase', onPurchase);
+        return () => Iaphub.end?.()
+    },[]);
+    return <IaphubDataProvider {...{ appId: "A", apiKey: "A", userId: "A", lang:l10n.getLanguage()}} {...props} />;
+}
+
+
+;(function({type:IntroPhase}){
+    IntroPhase.prototype.getPhaseText=(_get=>function(){
+        const {introPhase}=this.props
+        if(introPhase.title)
+            return introPhase.title
+        return _get.call(this)
+    })(IntroPhase.prototype.getPhaseText)
+})(require("react-native-iaphub-ui/src/paywall/intro-phase").default({}));
---
+++ tools/make-localized.js
@@ -0,0 +1,41 @@
+import LocalizedStrings from 'react-native-localization'
+
+export default function makeLocalized(data,lang){
+    const strings=new LocalizedStrings({
+        en:{
+            localizeNumber(d){
+                if(-1!==d.indexOf("."))
+                    return d
+                d=parseInt(d)
+                if(d>1000 && Math.floor(d/1000)*1000 == d){
+                    return `${d/1000}K`
+                }
+                return d 
+            }
+        },
+        ...data
+    })
+    
+    function printf(str){
+        const numbers=[]
+        const key=str.replace(/\d+(\.\d+)?/g,match=>(numbers.push(match),"%d"))
+        if(numbers.length==0)
+            return str
+    
+        let localized=strings[key]
+        if(!localized){
+            localized=key
+        }
+        return localized.replace(new RegExp(`%d`,'g'),()=>strings.localizeNumber(numbers.shift()))
+    }
+    
+    if(lang){
+        strings.setLanguage(lang)
+    }
+    
+    return new Proxy(strings,{
+        get(targets, key){
+            return targets[key]||printf(key)
+        }
+    })
+}

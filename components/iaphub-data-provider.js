a7b0dba34ac09de9286af76eb24cf575beeb834f
lalalic <lalalic@139.com>
2023-07-30 13:38:14 -0400

    extended

--- components/iaphub-data-provider.js
+++ components/iaphub-data-provider.js
@@ -22,32 +22,6 @@ export default function MyIaphubDataProvider({products,onPurchase, l10n, ...prop
     return <IaphubDataProvider {...{ appId: "A", apiKey: "A", userId: "A", lang:l10n.getLanguage()}} {...props} />;
 }
 
-
-import React from "react";
-import Iaphub from "react-native-iaphub"
-import { IaphubDataProvider } from 'react-native-iaphub-ui';
-import iap from "react-native-iaphub-ui/src/i18n/en"
-
-export default function MyIaphubDataProvider({products,onPurchase, l10n, ...props}) {
-    Iaphub.products=products
-
-    React.useMemo(()=>{
-        Object.keys(iap).forEach(key=>{
-            Object.keys(iap[key]).forEach(k=>{
-                iap[key][k]=(fx=>(...args)=>l10n[fx(...args)])(iap[key][k])
-            })
-        })
-        return true
-    },[])
-
-    React.useEffect(() =>{
-        onPurchase && Iaphub.addEventListener('onPurchase', onPurchase);
-        return () => Iaphub.end?.()
-    },[]);
-    return <IaphubDataProvider {...{ appId: "A", apiKey: "A", userId: "A", lang:l10n.getLanguage()}} {...props} />;
-}
-
-
 ;(function({type:IntroPhase}){
     IntroPhase.prototype.getPhaseText=(_get=>function(){
         const {introPhase}=this.props
